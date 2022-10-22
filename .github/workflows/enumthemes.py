6d146b155561e516df539127d0b8c7308aef6bf9
Abdelrhman-AK <59510211+Abdelrhman-AK@users.noreply.github.com>
2022-10-22 19:16:33 +0200

    Create enumthemes.py

---
+++ .github/workflows/enumthemes.py
@@ -0,0 +1,29 @@
+import os 
+ import sys 
+  
+  
+ def set_action_output(name: str, value: str): 
+     sys.stdout.write(f'::set-output name={name}::{value}\n') 
+  
+  
+ def main(): 
+     path = 'Themes'
+     extension = '.wptp'
+  
+     path_count = 0 
+     paths = '' 
+     for root, dirs, files in os.walk(path): 
+         for file in files: 
+             if file.endswith(f'{extension}'): 
+                 paths = paths + root + '/' + str(file) + ' ' 
+                 path_count = path_count + 1 
+  
+     set_action_output('path_count', path_count) 
+     set_action_output('paths', paths) 
+     print(paths) 
+  
+     sys.exit(0) 
+  
+  
+ if __name__ == "__main__": 
+     main()
