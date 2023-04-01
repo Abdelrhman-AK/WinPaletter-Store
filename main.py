b7eee320db64e2ba46d61b59dca7b8b9427deec8
Abdelrhman-AK <59510211+Abdelrhman-AK@users.noreply.github.com>
2023-04-01 17:19:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import sys
 from datetime import datetime
 from git.repo import Repo
 import git
-    
+import hashlib
+
 def set_action_output(name: str, value: str):
     with open(os.environ["GITHUB_OUTPUT"], "a") as myfile:
         myfile.write(f"{name}={value}\n")
@@ -23,7 +24,7 @@ def main():
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith(f'{extension}'):
-                paths = paths + root + '/' + str(file) + ' '
+                paths = paths + hashlib.md5(data).hexdigest(str(file)) + '|' + root + '/' + str(file) + ' '
                 path_count = path_count + 1
 
     set_action_output('path_count', path_count)
