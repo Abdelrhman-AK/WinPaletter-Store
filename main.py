9dad9e54f6527a437a3c679a723c2877ca241cb7
Abdelrhman-AK <59510211+Abdelrhman-AK@users.noreply.github.com>
2023-04-01 16:31:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,23 @@ def set_action_output(name: str, value: str):
         myfile.write(f"{name}={value}\n")
 
 def main():
-    set_action_output('Hello', 'Hello')
-    print('Hello')
+    path = sys.argv[0]
+    extension = sys.argv[1]
+
+    path_count = 0
+    paths = ''
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(f'{extension}'):
+                paths = paths + root + '/' + str(file) + ' '
+                path_count = path_count + 1
+
+    set_action_output('path_count', path_count)
+    set_action_output('paths', paths)
+    print(paths)
 
     sys.exit(0)
+
+
 if __name__ == "__main__":
     main()
