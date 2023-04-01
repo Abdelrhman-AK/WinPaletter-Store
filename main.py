109c64f521527ea1444981f2302f6f3d0b362203
Abdelrhman-AK <59510211+Abdelrhman-AK@users.noreply.github.com>
2023-04-01 16:22:14 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+
+def set_action_output(name: str, value: str):
+    with open(os.environ["GITHUB_OUTPUT"], "a") as myfile:
+        myfile.write(f"{name}={value}\n")
+
+def main():
+    path = os.environ["INPUT_PATH"]
+    extension = os.environ["INPUT_TYPE"]
+
+    path_count = 0
+    paths = ''
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(f'{extension}'):
+                paths = paths + root + '/' + str(file) + ' '
+                path_count = path_count + 1
+
+    set_action_output('path_count', path_count)
+    set_action_output('paths', paths)
+    print('Hello')
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
